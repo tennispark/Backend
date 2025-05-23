547647a900f2d595489236519801d78ea2b2c5a5
msk226 <kminseok5167@gmail.com>
2025-05-23 21:00:38 +0900

    chore: 초기 파일 업로드

---
+++ src/main/java/kr/tennispark/TennisParkApplication.java
@@ -0,0 +1,13 @@
+package kr.tennispark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennisParkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/tennispark/TennisParkApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.tennispark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisParkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
