93fdc4d4516cffbc3fcb9dbc74e7ff1679ab9fee
msk226 <kminseok5167@gmail.com>
2025-05-26 21:52:51 +0900

    feat: 포인트 검증 로직 추가

--- src/main/java/kr/tennispark/event/domain/Event.java
+++ src/main/java/kr/tennispark/event/domain/Event.java
@@ -3,6 +3,7 @@ package kr.tennispark.event.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import kr.tennispark.common.domain.BaseEntity;
+import kr.tennispark.event.domain.exception.InvalidEventException;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -31,6 +32,13 @@ public class Event extends BaseEntity {
     private String imageUrl;
 
     public static Event of(String name, String detail, Integer point, String imageUrl) {
+        validatePoint(point);
         return new Event(name, detail, point, imageUrl);
+    }
+
+    public static void validatePoint(Integer point) {
+        if (point < 0) {
+            throw new InvalidEventException("포인트는 0 이상이어야 합니다.");
+        }
     }
 }
