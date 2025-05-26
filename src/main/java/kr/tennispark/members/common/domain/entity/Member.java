6a698aa60ea2f8fe5ab55e6b9d17645264f28866
yooookm <yookyungmi02@gmail.com>
2025-05-27 02:13:35 +0900

    feat: Member 도메인 엔티티 생성

---
+++ src/main/java/kr/tennispark/members/common/domain/entity/Member.java
@@ -0,0 +1,62 @@
+package kr.tennispark.members.common.domain.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import kr.tennispark.common.domain.BaseEntity;
+import kr.tennispark.members.common.domain.entity.enums.Gender;
+import kr.tennispark.members.common.domain.entity.enums.JoinPath;
+import kr.tennispark.members.common.domain.entity.enums.MemberShipType;
+import kr.tennispark.members.common.domain.entity.vo.Email;
+import kr.tennispark.members.common.domain.entity.vo.Phone;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@SQLDelete(sql = "UPDATE member SET status = false WHERE id = ?")
+@SQLRestriction("status = true")
+public class Member extends BaseEntity {
+
+    @Embedded
+    private Email email;
+
+    @Embedded
+    private Phone phone;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int year;
+
+    @Column(nullable = false)
+    private String career;
+
+    @Column(nullable = false)
+    private String recommender;
+
+    @Column(nullable = false)
+    private String instagramId;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @ColumnDefault("GUEST")
+    private MemberShipType memberShipType = MemberShipType.GUEST;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private JoinPath joinPath;
+}
---
+++ src/main/java/kr/tennispark/members/common/domain/entity/enums/JoinPath.java
@@ -0,0 +1,6 @@
+package kr.tennispark.members.common.domain.entity.enums;
+
+public enum JoinPath {
+
+    INSTAGRAM, NAVER_SEARCH, FRIEND_RECOMMENDATION
+}
