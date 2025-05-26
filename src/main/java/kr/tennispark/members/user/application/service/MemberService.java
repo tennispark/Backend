18768e569f10bf997a07d9f4fcf362f95c5cfd92
yooookm <yookyungmi02@gmail.com>
2025-05-27 02:16:25 +0900

    feat: Member Service 생성

---
+++ src/main/java/kr/tennispark/members/user/application/service/MemberService.java
@@ -0,0 +1,14 @@
+package kr.tennispark.members.user.application.service;
+
+import kr.tennispark.members.user.infrastructure.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+}
