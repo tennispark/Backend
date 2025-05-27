8b95f084ddcb16c6ccd32aa92adf777b61f4514e
yooookm <yookyungmi02@gmail.com>
2025-05-28 01:51:44 +0900

    feat: 문자 전송 실패 커스텀 익셉션 생성

--- src/main/java/kr/tennispark/auth/presentation/exception/AuthExceptionHandler.java
+++ src/main/java/kr/tennispark/auth/presentation/exception/AuthExceptionHandler.java
@@ -3,6 +3,7 @@ package kr.tennispark.auth.presentation.exception;
 import kr.tennispark.auth.application.exception.MemberAlreadyExistsException;
 import kr.tennispark.auth.application.exception.PhoneNotVerifiedException;
 import kr.tennispark.auth.application.exception.PhoneVerificationFailedException;
+import kr.tennispark.auth.infrastructure.exception.SmsSendFailedException;
 import kr.tennispark.common.utils.ApiUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,6 +27,12 @@ public class AuthExceptionHandler {
     @ExceptionHandler(MemberAlreadyExistsException.class)
     public ResponseEntity<ApiUtils.ApiResult<?>> memberAlreadyExistsException(
             MemberAlreadyExistsException exception) {
+        return new ResponseEntity<>(exception.body(), exception.status());
+    }
+
+    @ExceptionHandler(SmsSendFailedException.class)
+    public ResponseEntity<ApiUtils.ApiResult<?>> smsSendFailedException(
+            SmsSendFailedException exception) {
         return new ResponseEntity<>(exception.body(), exception.status());
     }
 }
