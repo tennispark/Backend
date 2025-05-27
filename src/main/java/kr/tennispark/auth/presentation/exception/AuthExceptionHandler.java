2e23c99b0e0f663fa3eb7b826b897235b884c8e4
yooookm <yookyungmi02@gmail.com>
2025-05-28 00:39:11 +0900

    feat: 전화번호 인증 실패 커스텀 에러 생성

---
+++ src/main/java/kr/tennispark/auth/presentation/exception/AuthExceptionHandler.java
@@ -0,0 +1,17 @@
+package kr.tennispark.auth.presentation.exception;
+
+import kr.tennispark.auth.application.exception.PhoneVerificationFailedException;
+import kr.tennispark.common.utils.ApiUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class AuthExceptionHandler {
+
+    @ExceptionHandler(PhoneVerificationFailedException.class)
+    public ResponseEntity<ApiUtils.ApiResult<?>> phoneVerificationFailedException(
+            PhoneVerificationFailedException exception) {
+        return new ResponseEntity<>(exception.body(), exception.status());
+    }
+}
