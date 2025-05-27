657cec671c370808f127ce01da71a5e15461e394
yooookm <yookyungmi02@gmail.com>
2025-05-28 00:26:52 +0900

    chore: 핸드폰 인증 api request 이름 변경

--- src/main/java/kr/tennispark/auth/presentation/dto/request/VerifyPhoneRequest.java
+++ src/main/java/kr/tennispark/auth/presentation/dto/request/VerifyPhoneRequest.java
@@ -3,7 +3,7 @@ package kr.tennispark.auth.presentation.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
-public record VerifyAuthCodeRequest(
+public record VerifyPhoneRequest(
 
         @NotBlank(message = "휴대폰 번호는 필수입니다.")
         @Pattern(regexp = "^010\\d{8}$", message = "휴대폰 번호는 010으로 시작하고 숫자 8자여야 합니다.")
