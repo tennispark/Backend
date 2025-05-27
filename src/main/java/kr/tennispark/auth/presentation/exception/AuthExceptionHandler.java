ec0e7a4c35c3a05e0e794f67e6090240539c8918
yooookm <yookyungmi02@gmail.com>
2025-05-28 01:13:37 +0900

    feat: 핸드폰 인증 전 커스텀 에러 생성

--- src/main/java/kr/tennispark/auth/presentation/exception/AuthExceptionHandler.java
+++ src/main/java/kr/tennispark/auth/presentation/exception/AuthExceptionHandler.java
@@ -1,5 +1,6 @@
 package kr.tennispark.auth.presentation.exception;
 
+import kr.tennispark.auth.application.exception.PhoneNotVerifiedException;
 import kr.tennispark.auth.application.exception.PhoneVerificationFailedException;
 import kr.tennispark.common.utils.ApiUtils;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,12 @@ public class AuthExceptionHandler {
     @ExceptionHandler(PhoneVerificationFailedException.class)
     public ResponseEntity<ApiUtils.ApiResult<?>> phoneVerificationFailedException(
             PhoneVerificationFailedException exception) {
+        return new ResponseEntity<>(exception.body(), exception.status());
+    }
+
+    @ExceptionHandler(PhoneNotVerifiedException.class)
+    public ResponseEntity<ApiUtils.ApiResult<?>> phoneNotVerifiedException(
+            PhoneNotVerifiedException exception) {
         return new ResponseEntity<>(exception.body(), exception.status());
     }
 }
