9bdfca2f12a2fc5bcdb6a6a538061e2fac9705d4
yooookm <yookyungmi02@gmail.com>
2025-05-29 01:26:27 +0900

    feat: jwt claim 조회 기능 추가

--- src/main/java/kr/tennispark/auth/application/service/JwtTokenProvider.java
+++ src/main/java/kr/tennispark/auth/application/service/JwtTokenProvider.java
@@ -6,10 +6,13 @@ import java.time.Duration;
 import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -43,8 +46,18 @@ public class JwtTokenProvider {
                 .claim(ROLE_CLAIM, role)
                 .build();
 
-        return jwtEncoder.encode(JwtEncoderParameters.from(claims))
+        JwsHeader jwsHeader = JwsHeader.with(MacAlgorithm.HS256).build();
+        JwtEncoderParameters params = JwtEncoderParameters.from(jwsHeader, claims);
+
+        return jwtEncoder.encode(params)
                 .getTokenValue();
     }
 
+    public String getSubject(String token) {
+        try {
+            return jwtDecoder.decode(token).getSubject();
+        } catch (JwtException e) {
+            throw new RuntimeException("토큰이 이상해요");
+        }
+    }
 }
