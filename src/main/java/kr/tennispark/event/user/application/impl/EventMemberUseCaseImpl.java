420991875c27581984e5b88e99c5ad7f0126c198
msk226 <kminseok5167@gmail.com>
2025-05-29 21:25:58 +0900

    feat: 이벤트 참여에 따른 포인트 증가 로직 구현

--- src/main/java/kr/tennispark/event/user/application/impl/EventMemberUseCaseImpl.java
+++ src/main/java/kr/tennispark/event/user/application/impl/EventMemberUseCaseImpl.java
@@ -7,7 +7,11 @@ import kr.tennispark.event.user.application.EventMemberUseCase;
 import kr.tennispark.event.user.application.exception.AlreadyAttendEventException;
 import kr.tennispark.event.user.infrastructure.repository.EventApplicationRepository;
 import kr.tennispark.members.common.domain.entity.Member;
-import kr.tennispark.members.user.application.service.MemberService;
+import kr.tennispark.members.common.domain.entity.enums.PointReason;
+import kr.tennispark.members.common.domain.entity.vo.Point;
+import kr.tennispark.members.common.domain.entity.vo.PointHistory;
+import kr.tennispark.members.user.infrastructure.repository.PointHistoryRepository;
+import kr.tennispark.members.user.infrastructure.repository.PointRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,26 +23,33 @@ public class EventMemberUseCaseImpl implements EventMemberUseCase {
 
     private final EventRepository eventRepository;
     private final EventApplicationRepository eventApplicationRepository;
-    private final MemberService memberService;
+    private final PointRepository pointRepository;
+    private final PointHistoryRepository pointHistoryRepository;
 
     @Override
     public void attendEvent(Long eventId, Member member) {
         Event event = eventRepository.getById(eventId);
-        checkIsAttendEvent(event, member);
+        validateNotAlreadyAttended(event, member);
 
-        memberService.addMemberPoint(member, event.getPoint());
-        saveEventApplication(member, event);
-
+        applyForEvent(member, event);
+        earnEventPoint(member, event);
     }
 
-    private void saveEventApplication(Member member, Event event) {
-        EventApplication eventApplication = EventApplication.of(event, member);
-        eventApplicationRepository.save(eventApplication);
-    }
-
-    private void checkIsAttendEvent(Event event, Member member) {
+    private void validateNotAlreadyAttended(Event event, Member member) {
         if (eventApplicationRepository.existsByEventAndMember(event, member)) {
             throw new AlreadyAttendEventException();
         }
+    }
+
+    private void applyForEvent(Member member, Event event) {
+        EventApplication application = EventApplication.of(event, member);
+        eventApplicationRepository.save(application);
+    }
+
+    private void earnEventPoint(Member member, Event event) {
+        Point point = pointRepository.getByMemberId(member.getId());
+
+        point.addPoint(event.getPoint());
+        pointHistoryRepository.save(PointHistory.of(point, event.getPoint(), PointReason.EVENT));
     }
 }
