b2504ff30c2378bb1578c1c2c2fc06701b05fa24
yooookm <yookyungmi02@gmail.com>
2025-05-30 21:06:55 +0900

    feat: "이번 주" 계산 로직  vo 생성

--- src/main/java/kr/tennispark/activity/common/domain/enums/Days.java
+++ src/main/java/kr/tennispark/activity/common/domain/enums/Days.java
@@ -1,5 +1,6 @@
 package kr.tennispark.activity.common.domain.enums;
 
+import java.time.DayOfWeek;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
@@ -21,5 +22,17 @@ public enum Days {
         } catch (IllegalArgumentException e) {
             throw new InvalidActivityTimeException("유효하지 않은 활동 요일이 포함되어 있습니다.");
         }
+    }
+
+    public DayOfWeek toDayOfWeek() {
+        return switch (this) {
+            case MON -> DayOfWeek.MONDAY;
+            case TUE -> DayOfWeek.TUESDAY;
+            case WED -> DayOfWeek.WEDNESDAY;
+            case THU -> DayOfWeek.THURSDAY;
+            case FRI -> DayOfWeek.FRIDAY;
+            case SAT -> DayOfWeek.SATURDAY;
+            case SUN -> DayOfWeek.SUNDAY;
+        };
     }
 }
