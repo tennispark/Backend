344275e57494b76555b3f2e4c0d62ccf8404e607
yooookm <yookyungmi02@gmail.com>
2025-05-30 22:21:12 +0900

    fix: 코트 타입 유효성 어노테이션 수정

--- src/main/java/kr/tennispark/activity/admin/presentation/dto/request/ManageActivityInfoRequestDTO.java
+++ src/main/java/kr/tennispark/activity/admin/presentation/dto/request/ManageActivityInfoRequestDTO.java
@@ -12,7 +12,7 @@ public record ManageActivityInfoRequestDTO(
         @NotNull(message = "활동 종료 시간은 필수입니다.") LocalTime endAt,
         @NotEmpty(message = "활동 반복 요일은 필수입니다.") List<String> activeDays,
         @NotNull(message = "활동 참여 인원은 필수입니다.") Integer participantCount,
-        @NotBlank(message = "코트 타입은 필수입니다.") CourtType courtName,
+        @NotNull(message = "코트 타입은 필수입니다.") CourtType courtType,
         @NotBlank(message = "활동 장소 이름은 필수입니다.") String placeName,
         @NotBlank(message = "활동 장소 주소는 필수입니다.") String address,
         @NotNull(message = "활동 반복 유무는 필수입니다.") Boolean isRecurring
