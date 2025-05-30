db28192ceea67a74461b0fc04aca92af88ac31f1
msk226 <kminseok5167@gmail.com>
2025-05-30 17:28:35 +0900

    feat: Multipart 이미지 업로드 메서드 구현

--- src/main/java/kr/tennispark/qr/application/S3UploadService.java
+++ src/main/java/kr/tennispark/qr/application/S3UploadService.java
@@ -4,19 +4,22 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 public class S3UploadService {
 
-    private static final String IMAGE_TYPE = "image/png";
-    private static final String FILE_EXTENSION = ".png";
+    private static final String DEFAULT_CONTENT_TYPE = "image/png";
+    private static final String DEFAULT_EXTENSION = ".png";
     private static final String SLASH = "/";
-
 
     private final AmazonS3 amazonS3;
 
@@ -24,12 +27,27 @@ public class S3UploadService {
     private String bucket;
 
     public String uploadQrImage(byte[] imageData, String keyPrefix) {
-        String fileName = keyPrefix + SLASH + UUID.randomUUID() + FILE_EXTENSION;
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentType(IMAGE_TYPE);
-        metadata.setContentLength(imageData.length);
+        return uploadToS3(new ByteArrayInputStream(imageData), imageData.length, keyPrefix, DEFAULT_CONTENT_TYPE,
+                DEFAULT_EXTENSION);
+    }
 
-        amazonS3.putObject(new PutObjectRequest(bucket, fileName, new ByteArrayInputStream(imageData), metadata));
+    public String uploadAdvertisementImage(MultipartFile file, String keyPrefix) {
+        try {
+            return uploadToS3(file.getInputStream(), file.getSize(), keyPrefix, file.getContentType(),
+                    DEFAULT_EXTENSION);
+        } catch (IOException e) {
+            throw new UncheckedIOException("광고 배너 이미지 업로드에 실패하였습니다.", e);
+        }
+    }
+
+    private String uploadToS3(InputStream inputStream, long contentLength, String keyPrefix, String contentType,
+                              String extension) {
+        String fileName = keyPrefix + SLASH + UUID.randomUUID() + extension;
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(contentType);
+        metadata.setContentLength(contentLength);
+
+        amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, metadata));
         return amazonS3.getUrl(bucket, fileName).toString();
     }
 }
