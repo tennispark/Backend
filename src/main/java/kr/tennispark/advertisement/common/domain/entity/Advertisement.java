22753713e80f92bbcb8d09976b28c2c95d662de1
msk226 <kminseok5167@gmail.com>
2025-05-30 16:29:53 +0900

    feat: 광고 배너 도메인 엔티티 생성

---
+++ src/main/java/kr/tennispark/advertisement/common/domain/entity/Advertisement.java
@@ -0,0 +1,31 @@
+package kr.tennispark.advertisement.common.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import kr.tennispark.advertisement.common.domain.entity.enums.Position;
+import kr.tennispark.common.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@SQLDelete(sql = "UPDATE advertisement SET status = false WHERE id = ?")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@SQLRestriction("status = true")
+public class Advertisement extends BaseEntity {
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Position position;
+
+}
