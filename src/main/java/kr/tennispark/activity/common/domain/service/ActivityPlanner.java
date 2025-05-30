aae638779741f0735c589342fc8265a992d56f5d
yooookm <yookyungmi02@gmail.com>
2025-05-30 21:16:33 +0900

    feat: activityInfo로 activity List 생성 로직 구현

---
+++ src/main/java/kr/tennispark/activity/common/domain/service/ActivityPlanner.java
@@ -0,0 +1,28 @@
+package kr.tennispark.activity.common.domain.service;
+
+import java.util.List;
+import kr.tennispark.activity.admin.infrastructure.repository.ActivityRepository;
+import kr.tennispark.activity.common.domain.Activity;
+import kr.tennispark.activity.common.domain.ActivityInfo;
+import kr.tennispark.activity.common.domain.vo.WeekPeriod;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ActivityPlanner {
+
+    private final ActivityRepository activityRepo;
+
+    public List<Activity> plan(ActivityInfo info, WeekPeriod week) {
+
+        return info.getActTime().getActiveDays().stream()
+                .map(week::toDate)
+                .filter(week::contains)
+                .filter(date ->
+                        !activityRepo.existsByTemplateAndDate(info, date))
+                .map(date -> Activity.of(info, date))
+                .toList();
+    }
+}
+
