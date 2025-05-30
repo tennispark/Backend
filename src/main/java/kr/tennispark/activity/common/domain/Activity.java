49842cca5f7a81cfcbe57f9dce19f6048dbd3b5f
yooookm <yookyungmi02@gmail.com>
2025-05-30 20:32:41 +0900

    feat: 실제 활동 객체인 activity 도메인 구현

---
+++ src/main/java/kr/tennispark/activity/common/domain/Activity.java
@@ -0,0 +1,66 @@
+package kr.tennispark.activity.common.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+import kr.tennispark.activity.common.domain.enums.CourtType;
+import kr.tennispark.activity.common.domain.vo.Place;
+import kr.tennispark.activity.common.domain.vo.ScheduledTime;
+import kr.tennispark.common.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@SQLDelete(sql = "UPDATE activity SET status=false WHERE id=?")
+@SQLRestriction("status=true")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Activity extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "info_id")
+    private ActivityInfo template;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Embedded
+    private ScheduledTime scheduledTime;
+
+    @Column(nullable = false)
+    private Integer participantCount = 0;
+
+    @Column(nullable = false)
+    private Integer capacity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CourtType courtType;
+
+    @Embedded
+    private Place place;
+
+    public static Activity of(ActivityInfo info, LocalDate date) {
+        return new Activity(
+                info,
+                date,
+                info.getActTime(),
+                0,
+                info.getCapacity(),
+                info.getCourtType(),
+                info.getPlace()
+        );
+    }
+}
+
