56cd2066b6a1b72be08c065dda38524b56a23844
yooookm <yookyungmi02@gmail.com>
2025-05-31 01:47:19 +0900

    feat: 활동 조회 기능 함수 추가 - 비관적 락 적용

---
+++ src/main/java/kr/tennispark/activity/user/infrastructure/repository/UserActivityRepository.java
@@ -0,0 +1,18 @@
+package kr.tennispark.activity.user.infrastructure.repository;
+
+import jakarta.persistence.LockModeType;
+import java.util.Optional;
+import kr.tennispark.activity.common.domain.Activity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserActivityRepository extends JpaRepository<Activity, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select a from Activity a where a.id = :id and a.status = true")
+    Optional<Activity> findForUpdate(@Param("id") Long id);
+}
