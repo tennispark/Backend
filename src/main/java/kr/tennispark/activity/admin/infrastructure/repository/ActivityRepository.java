ef7de89416803aee1e383a07386ca163dda3a243
yooookm <yookyungmi02@gmail.com>
2025-05-30 21:16:04 +0900

    feat: 특정 날짜와 activityInfo를 가진 activity 유무 조회

---
+++ src/main/java/kr/tennispark/activity/admin/infrastructure/repository/ActivityRepository.java
@@ -0,0 +1,14 @@
+package kr.tennispark.activity.admin.infrastructure.repository;
+
+import java.time.LocalDate;
+import kr.tennispark.activity.common.domain.Activity;
+import kr.tennispark.activity.common.domain.ActivityInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ActivityRepository
+        extends JpaRepository<Activity, Long> {
+
+    boolean existsByTemplateAndDate(ActivityInfo template, LocalDate date);
+}
