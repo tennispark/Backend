b396c5e17a9fe322ca426b5908d6a5a4d762d539
yooookm <yookyungmi02@gmail.com>
2025-05-30 21:12:39 +0900

    feat: ActivityInfoRepository에 @Repository 어노테이션 추가

--- src/main/java/kr/tennispark/activity/admin/infrastructure/repository/ActivityInfoRepository.java
+++ src/main/java/kr/tennispark/activity/admin/infrastructure/repository/ActivityInfoRepository.java
@@ -5,7 +5,9 @@ import kr.tennispark.activity.common.domain.exception.NoSuchActivityException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ActivityInfoRepository extends JpaRepository<ActivityInfo, Long> {
 
     Page<ActivityInfo> findAllByOrderByCreatedAtDesc(Pageable pageable);
