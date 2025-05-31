3df97bdf6e089f7012e74eced201c932138c3e8e
Yoo KyeongMi <yookyungmi02@gmail.com>
2025-05-31 18:50:25 +0900

    feat: activity 도메인에 활동 이름 필드 추가, 코트 타입 필드 삭제 - academy와 일반 활동이 함께 activity 도메인을 쓰기 위해 (코트 타입, 수업 유형)을 활동 이름으로 통일

--- src/main/java/kr/tennispark/activity/common/domain/ActivityInfo.java
+++ src/main/java/kr/tennispark/activity/common/domain/ActivityInfo.java
@@ -11,6 +11,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import java.time.LocalTime;
 import java.util.List;
+import kr.tennispark.activity.common.domain.enums.ActivityName;
 import kr.tennispark.activity.common.domain.enums.ActivityType;
 import kr.tennispark.activity.common.domain.enums.CourtType;
 import kr.tennispark.activity.common.domain.enums.Days;
@@ -32,10 +33,6 @@ import org.hibernate.annotations.SQLRestriction;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @SQLRestriction("status = true")
 public class ActivityInfo extends BaseEntity {
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private CourtType courtType;
 
     @Embedded
     private Place place;
@@ -61,26 +58,34 @@ public class ActivityInfo extends BaseEntity {
     @ColumnDefault("'GENERAL'")
     private ActivityType type = ActivityType.GENERAL;
 
-    public static ActivityInfo of(CourtType courtType, String placeName, String address, LocalTime beginAt,
-                                  LocalTime endAt,
-                                  List<String> activeDays, Boolean isRecurring, Integer capacity) {
-        return new ActivityInfo(courtType,
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ActivityName activityName;
+
+    public static ActivityInfo of(ActivityName activityName,String placeName, String address,
+                                  LocalTime beginAt, LocalTime endAt,
+                                  List<String> activeDays, Boolean isRecurring,
+                                  Integer capacity) {
+        activityName.validateWith(ActivityType.GENERAL);
+        return new ActivityInfo(
                 Place.of(placeName, address),
                 ScheduledTime.of(beginAt, endAt),
                 Days.from(activeDays),
                 isRecurring,
                 capacity,
-                ActivityType.GENERAL);
+                ActivityType.GENERAL,
+                activityName);
     }
 
     public void modifyActivityInfoDetails(
-            CourtType courtType, String placeName, String address, LocalTime beginAt, LocalTime endAt,
+            ActivityName activityName, String placeName, String address, LocalTime beginAt, LocalTime endAt,
             List<String> activeDays, Boolean isRecurring, Integer capacity) {
-        this.courtType = courtType;
+        activityName.validateWith(type);
         this.place = Place.of(placeName, address);
         this.time = ScheduledTime.of(beginAt, endAt);
         this.activeDays = Days.from(activeDays);
         this.isRecurring = isRecurring;
         this.capacity = capacity;
+        this.activityName = activityName;
     }
 }
