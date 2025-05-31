732a6780e0b9b204d7a0ca8b01303506ce639def
Yoo KyeongMi <yookyungmi02@gmail.com>
2025-06-01 00:15:44 +0900

    feat: 상품 도메인 작성

---
+++ src/main/java/kr/tennispark/point/common/domain/entity/Product.java
@@ -0,0 +1,28 @@
+package kr.tennispark.point.common.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import kr.tennispark.common.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@SQLDelete(sql = "UPDATE Product SET status = false WHERE id = ?")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLRestriction("status = true")
+public class Product extends BaseEntity {
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer amount;
+
+    @Column(nullable = false)
+    private String imageUrl;
+}
