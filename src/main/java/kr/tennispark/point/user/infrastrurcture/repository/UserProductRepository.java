095b6f9b29d6ffcc3583890b16ff59a45d9c4108
Yoo KyeongMi <yookyungmi02@gmail.com>
2025-06-01 03:40:34 +0900

    feat: 상품 id로 상품 조회 구현

--- src/main/java/kr/tennispark/point/user/infrastrurcture/repository/UserProductRepository.java
+++ src/main/java/kr/tennispark/point/user/infrastrurcture/repository/UserProductRepository.java
@@ -1,9 +1,15 @@
 package kr.tennispark.point.user.infrastrurcture.repository;
 
 import kr.tennispark.point.common.domain.entity.Product;
+import kr.tennispark.point.common.domain.exception.NoSuchProductException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserProductRepository extends JpaRepository<Product, Long> {
+
+    default Product getById(Long id) {
+        return findById(id)
+                .orElseThrow(NoSuchProductException::new);
+    }
 }
