fd48630f59d48121b6513b671b68c8f0da98123b
Yoo KyeongMi <yookyungmi02@gmail.com>
2025-05-31 18:52:03 +0900

    feat: 활동 등록/수정 dto 수정 - 코트 타입 필드 자료형을 ActivityName으로 변경 - 클라이언트 혼란 최소화를 위해 필드명은 그대로 둠

--- src/main/java/kr/tennispark/activity/admin/presentation/dto/request/ManageActivityInfoRequestDTO.java
+++ src/main/java/kr/tennispark/activity/admin/presentation/dto/request/ManageActivityInfoRequestDTO.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalTime;
 import java.util.List;
+import kr.tennispark.activity.common.domain.enums.ActivityName;
 import kr.tennispark.activity.common.domain.enums.CourtType;
 
 public record ManageActivityInfoRequestDTO(
@@ -12,7 +13,7 @@ public record ManageActivityInfoRequestDTO(
         @NotNull(message = "활동 종료 시간은 필수입니다.") LocalTime endAt,
         @NotEmpty(message = "활동 반복 요일은 필수입니다.") List<String> activeDays,
         @NotNull(message = "활동 참여 인원은 필수입니다.") Integer participantCount,
-        @NotNull(message = "코트 타입은 필수입니다.") CourtType courtType,
+        @NotNull(message = "코트 타입은 필수입니다.") ActivityName courtType,
         @NotBlank(message = "활동 장소 이름은 필수입니다.") String placeName,
         @NotBlank(message = "활동 장소 주소는 필수입니다.") String address,
         @NotNull(message = "활동 반복 유무는 필수입니다.") Boolean isRecurring
