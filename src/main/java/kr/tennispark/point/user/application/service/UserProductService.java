a290733e0e808bb9a74065f349e302b1b63be9a2
Yoo KyeongMi <yookyungmi02@gmail.com>
2025-06-01 03:43:54 +0900

    feat: 상품 구매 qr 생성 api 구현

--- src/main/java/kr/tennispark/point/user/application/service/UserProductService.java
+++ src/main/java/kr/tennispark/point/user/application/service/UserProductService.java
@@ -1,10 +1,15 @@
 package kr.tennispark.point.user.application.service;
 
 import java.util.List;
+import kr.tennispark.activity.user.presentation.dto.request.PurchasePayload;
+import kr.tennispark.members.common.domain.entity.Member;
 import kr.tennispark.point.common.domain.entity.Product;
 import kr.tennispark.point.user.infrastrurcture.repository.UserProductRepository;
 import kr.tennispark.point.user.presentation.dto.response.GetAllProductResponse;
+import kr.tennispark.point.user.presentation.dto.response.PurchaseProductResponse;
+import kr.tennispark.qr.application.QrService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,9 +19,23 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserProductService {
 
     private final UserProductRepository productRepository;
+    private final QrService qrService;
+
+    @Value("${qr.url.product.suffix}")
+    private String qrUrlSuffix;
 
     public GetAllProductResponse getAllProducts() {
         List<Product> products = productRepository.findAll();
         return GetAllProductResponse.of(products);
+    }
+
+    public PurchaseProductResponse createPurchaseQr(long productId, Member member) {
+        Product product = productRepository.getById(productId);
+
+        PurchasePayload payload = new PurchasePayload(productId, member.getId());
+
+        String qrUrl = qrService.createPayloadQr(payload, qrUrlSuffix);
+
+        return PurchaseProductResponse.of(qrUrl, product);
     }
 }
--- src/main/java/kr/tennispark/point/user/presentation/controller/UserProductController.java
+++ src/main/java/kr/tennispark/point/user/presentation/controller/UserProductController.java
@@ -1,12 +1,17 @@
 package kr.tennispark.point.user.presentation.controller;
 
+import kr.tennispark.common.annotation.LoginMember;
 import kr.tennispark.common.utils.ApiUtils;
 import kr.tennispark.common.utils.ApiUtils.ApiResult;
+import kr.tennispark.members.common.domain.entity.Member;
 import kr.tennispark.point.user.application.service.UserProductService;
 import kr.tennispark.point.user.presentation.dto.response.GetAllProductResponse;
+import kr.tennispark.point.user.presentation.dto.response.PurchaseProductResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +23,16 @@ public class UserProductController {
     private final UserProductService productService;
 
     @GetMapping
-    public ResponseEntity<ApiResult<GetAllProductResponse>> getAllProducts(){
+    public ResponseEntity<ApiResult<GetAllProductResponse>> getAllProducts() {
         GetAllProductResponse response = productService.getAllProducts();
+        return ResponseEntity.ok(ApiUtils.success(response));
+    }
+
+    @PostMapping("/{productId}/purchase-qr")
+    public ResponseEntity<ApiResult<PurchaseProductResponse>> issue(
+            @LoginMember Member member,
+            @PathVariable Long productId) {
+        PurchaseProductResponse response = productService.createPurchaseQr(productId, member);
         return ResponseEntity.ok(ApiUtils.success(response));
     }
 }
