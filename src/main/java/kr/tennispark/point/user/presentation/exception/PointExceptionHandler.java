0e06a770fd9366e1df2ffa465925fd586c8f6209
Yoo KyeongMi <yookyungmi02@gmail.com>
2025-06-01 02:13:04 +0900

    feat: 포인트 연산 커스텀 익셉션 생성

---
+++ src/main/java/kr/tennispark/point/user/presentation/exception/PointExceptionHandler.java
@@ -0,0 +1,17 @@
+package kr.tennispark.point.user.presentation.exception;
+
+import feign.Response;
+import kr.tennispark.common.utils.ApiUtils.ApiResult;
+import kr.tennispark.point.user.application.exception.PointNegativeValueException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class PointExceptionHandler {
+
+    @ExceptionHandler(PointNegativeValueException.class)
+    public ResponseEntity<ApiResult<?>> handlePointNegativeValueException(PointNegativeValueException exception){
+        return new ResponseEntity<>(exception.body(), exception.status());
+    }
+}
