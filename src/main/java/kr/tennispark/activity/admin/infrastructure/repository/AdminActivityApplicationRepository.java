87306b957230496ad3384cb70b145c51199a5d7a
msk226 <kminseok5167@gmail.com>
2025-06-01 13:36:36 +0900

    feat: 활동 신청 조회 쿼리 페치 조인으로 변경

--- src/main/java/kr/tennispark/activity/admin/infrastructure/repository/AdminActivityApplicationRepository.java
+++ src/main/java/kr/tennispark/activity/admin/infrastructure/repository/AdminActivityApplicationRepository.java
@@ -7,11 +7,19 @@ import kr.tennispark.activity.common.domain.ActivityApplication;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AdminActivityApplicationRepository extends JpaRepository<ActivityApplication, Long> {
 
     Page<ActivityApplication> findAllByActivityOrderByCreatedAtDesc(Activity activity, Pageable pageable);
 
+    @Query("""
+                        SELECT aa
+                        FROM ActivityApplication aa
+                        JOIN FETCH aa.member m
+                        JOIN FETCH aa.activity a
+                        WHERE m.id = :memberId AND a.id = :activityId
+            """)
     Optional<ActivityApplication> findByMember_IdAndActivity_Id(Long memberId, Long activityId);
 
     default ActivityApplication getByMemberIdAndActivityId(Long memberId, Long activityId) {
