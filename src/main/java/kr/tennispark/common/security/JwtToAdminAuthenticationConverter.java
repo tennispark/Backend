93d9941a9ae0a703a5f1d8d2f9eb19987ff4e81f
yooookm <yookyungmi02@gmail.com>
2025-06-13 02:55:12 +0900

    feat: admin 전용 jwtAuthConverter 생성

---
+++ src/main/java/kr/tennispark/common/security/JwtToAdminAuthenticationConverter.java
@@ -0,0 +1,32 @@
+package kr.tennispark.common.security;
+
+
+import static kr.tennispark.common.constant.JwtConstants.ADMIN_ROLE_VALUE;
+import static kr.tennispark.common.constant.JwtConstants.ROLE_CLAIM;
+import static kr.tennispark.common.constant.JwtConstants.ROLE_PREFIX;
+
+import java.util.List;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtToAdminAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+    private static final String ADMIN_PRINCIPAL = "admin";
+
+    @Override
+    public AbstractAuthenticationToken convert(Jwt jwt) {
+        String role = jwt.getClaimAsString(ROLE_CLAIM);
+        if (!ADMIN_ROLE_VALUE.equals(role)) {
+            return null;
+        }
+
+        return new UsernamePasswordAuthenticationToken(
+                ADMIN_PRINCIPAL,
+                "N/A",
+                List.of(new SimpleGrantedAuthority(ROLE_PREFIX + ADMIN_ROLE_VALUE)));
+    }
+}
