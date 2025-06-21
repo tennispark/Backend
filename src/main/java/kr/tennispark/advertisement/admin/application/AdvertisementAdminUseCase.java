0c6a34e4a80cf4758ffddf4beea3a12636edd8ca
msk226 <kminseok5167@gmail.com>
2025-06-21 16:21:27 +0900

    feat: 광고 생성 및 수정 시, 이미지 입력 받아 업로드 하도록 로직 변경

--- src/main/java/kr/tennispark/advertisement/admin/application/AdvertisementAdminUseCase.java
+++ src/main/java/kr/tennispark/advertisement/admin/application/AdvertisementAdminUseCase.java
@@ -3,14 +3,15 @@ package kr.tennispark.advertisement.admin.application;
 import kr.tennispark.advertisement.admin.presentation.dto.request.SaveAdvertisementRequestDTO;
 import kr.tennispark.advertisement.admin.presentation.dto.response.GetAdvertisementResponseDTO;
 import kr.tennispark.advertisement.common.domain.entity.enums.Position;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface AdvertisementAdminUseCase {
 
-    void saveAdvertisement(SaveAdvertisementRequestDTO request);
+    void saveAdvertisement(SaveAdvertisementRequestDTO request, MultipartFile image);
 
     void deleteAdvertisement(Long advertisementId);
 
-    void updateAdvertisement(SaveAdvertisementRequestDTO request, Long advertisementId);
+    void updateAdvertisement(SaveAdvertisementRequestDTO request, MultipartFile image, Long advertisementId);
 
     GetAdvertisementResponseDTO getAdvertisementsByPosition(Position position);
 
--- src/main/java/kr/tennispark/advertisement/admin/presentation/dto/request/SaveAdvertisementRequestDTO.java
+++ src/main/java/kr/tennispark/advertisement/admin/presentation/dto/request/SaveAdvertisementRequestDTO.java
@@ -3,7 +3,6 @@ package kr.tennispark.advertisement.admin.presentation.dto.request;
 import kr.tennispark.advertisement.common.domain.entity.enums.Position;
 
 public record SaveAdvertisementRequestDTO(
-        String imageUrl,
         Position position,
         String linkUrl
 ) {
