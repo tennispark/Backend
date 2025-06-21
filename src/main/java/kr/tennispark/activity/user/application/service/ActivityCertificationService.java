2ab157fd7660a7f084009e2aaea00a17cfaf14e1
msk226 <kminseok5167@gmail.com>
2025-06-21 16:43:19 +0900

    feat: 활동 인증 API 스펙 수정 - 이미지 파일 자체를 입력 받도록 변경

--- src/main/java/kr/tennispark/activity/user/application/service/ActivityCertificationService.java
+++ src/main/java/kr/tennispark/activity/user/application/service/ActivityCertificationService.java
@@ -3,19 +3,25 @@ package kr.tennispark.activity.user.application.service;
 import kr.tennispark.activity.common.domain.ActivityCertification;
 import kr.tennispark.activity.user.infrastructure.repository.ActivityCertificationRepository;
 import kr.tennispark.members.common.domain.entity.Member;
+import kr.tennispark.qr.application.S3UploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ActivityCertificationService {
 
+    private static final String IMAGE_PREFIX = "cert/";
     private final ActivityCertificationRepository certificationRepository;
+    private final S3UploadService uploadService;
 
     @Transactional
-    public void certifyActivity(Member member, String imageUrl) {
+    public void certifyActivity(Member member, MultipartFile image) {
+        String imageUrl = uploadService.uploadImageFile(image, IMAGE_PREFIX);
+
         ActivityCertification cert =
                 ActivityCertification.of(member, imageUrl);
         certificationRepository.save(cert);
--- src/main/java/kr/tennispark/activity/user/presentation/controller/UserActivityCertificationController.java
+++ src/main/java/kr/tennispark/activity/user/presentation/controller/UserActivityCertificationController.java
@@ -1,8 +1,6 @@
 package kr.tennispark.activity.user.presentation.controller;
 
-import jakarta.validation.Valid;
 import kr.tennispark.activity.user.application.service.ActivityCertificationService;
-import kr.tennispark.activity.user.presentation.dto.request.PostCertificationRequest;
 import kr.tennispark.common.annotation.LoginMember;
 import kr.tennispark.common.utils.ApiUtils;
 import kr.tennispark.common.utils.ApiUtils.ApiResult;
@@ -10,9 +8,10 @@ import kr.tennispark.members.common.domain.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/members/activities/certifications")
@@ -24,9 +23,9 @@ public class UserActivityCertificationController {
     @PostMapping
     public ResponseEntity<ApiResult<String>> certify(
             @LoginMember Member member,
-            @Valid @RequestBody PostCertificationRequest request
+            @RequestPart MultipartFile image
     ) {
-        service.certifyActivity(member, request.imageUrl());
+        service.certifyActivity(member, image);
         return ResponseEntity.ok(ApiUtils.success());
     }
 }
