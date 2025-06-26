59cd8f3973448db655b72ca546b1b91f7e00011a
yooookm <yookyungmi02@gmail.com>
2025-06-26 15:53:02 +0900

    feat: db랑 랭킹 싱크 맞추기 스케줄러 도입

--- src/main/java/kr/tennispark/match/common/infrastructure/repository/impl/MatchPointRankingSynchronizer.java
+++ src/main/java/kr/tennispark/match/common/infrastructure/repository/impl/MatchPointRankingSynchronizer.java
@@ -8,28 +8,25 @@ import kr.tennispark.members.user.infrastructure.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.DefaultTypedTuple;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class MatchPointRankingScheduler {
+public class MatchPointRankingSynchronizer {
 
     private static final String KEY = "ranking:member:matchPoint";
 
     private final StringRedisTemplate redis;
     private final MemberRepository memberRepository;
 
-    @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
-    public void syncAllMatchPointRanking() {
+    public void sync() {
         List<Member> members = memberRepository.findAll();
-
         if (members.isEmpty()) {
             return;
         }
 
-        Set<TypedTuple<String>> tuples = members.stream()
+        Set<ZSetOperations.TypedTuple<String>> tuples = members.stream()
                 .map(m -> new DefaultTypedTuple<>(
                         m.getId().toString(),
                         m.getMatchPoint().doubleValue()
