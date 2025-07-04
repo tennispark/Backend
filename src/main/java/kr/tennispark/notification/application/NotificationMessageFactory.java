41e0fd2258e79b69b28d74bf520851033df20f03
msk226 <kminseok5167@gmail.com>
2025-07-04 13:36:16 +0900

    feat: 메세지 생성 팩토리 메서드 정의

---
+++ src/main/java/kr/tennispark/notification/application/NotificationMessageFactory.java
@@ -0,0 +1,48 @@
+package kr.tennispark.notification.application;
+
+import java.time.LocalTime;
+import java.util.List;
+import kr.tennispark.activity.common.domain.Activity;
+import kr.tennispark.notification.domain.entity.enums.NotificationType;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationMessageFactory {
+
+    private static final String BRAND_PREFIX = "(테니스파크)";
+    private static final String COMMON_SUFFIX = "님들과 테니스 활동 예정입니다. 다른분들을 위해 꼭 늦지 않고 도착해주세요.";
+    private static final String ONE_DAY_FORMAT = "%s 내일 %s %s %s에서 %s " + COMMON_SUFFIX;
+    private static final String ONE_HOUR_FORMAT = "%s 잠시후 %s %s %s에서 %s " + COMMON_SUFFIX;
+    private static final String RECRUIT_FORMAT = "%s 이번주 테니스파크 활동 추가모집합니다. %s %d석";
+    private static final String TIME_FORMAT = "%02d시";
+    private static final String DELIMITER = ", ";
+
+    public static String createMessage(NotificationType type, Activity activity, List<String> participantNames) {
+        String time = formatTime(activity.getScheduledTime().getBeginAt());
+        String place = activity.getPlace().getName();
+        String courtName = activity.getCourtName();
+        int remainingSeats = activity.getCapacity() - activity.getParticipantCount();
+
+        String names = formatNames(participantNames);
+
+        return switch (type) {
+            case ONE_DAY_BEFORE -> String.format(
+                    ONE_DAY_FORMAT, BRAND_PREFIX, time, place, courtName, names
+            );
+            case ONE_HOUR_BEFORE -> String.format(
+                    ONE_HOUR_FORMAT, BRAND_PREFIX, time, place, courtName, names
+            );
+            case RECRUIT_REMINDER -> String.format(
+                    RECRUIT_FORMAT, BRAND_PREFIX, place, remainingSeats
+            );
+        };
+    }
+
+    private static String formatTime(LocalTime time) {
+        return String.format(TIME_FORMAT, time.getHour());
+    }
+
+    private static String formatNames(List<String> names) {
+        return String.join(DELIMITER, names);
+    }
+}
