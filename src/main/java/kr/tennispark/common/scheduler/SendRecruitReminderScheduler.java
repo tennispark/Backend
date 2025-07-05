5466ccea3ea8f16cdfe6bbc95e77de4ad733fd28
msk226 <kminseok5167@gmail.com>
2025-07-05 22:06:24 +0900

    feat: 주 시작 기준을 금요일로 지정

--- src/main/java/kr/tennispark/common/scheduler/SendRecruitReminderScheduler.java
+++ src/main/java/kr/tennispark/common/scheduler/SendRecruitReminderScheduler.java
@@ -1,10 +1,9 @@
 package kr.tennispark.common.scheduler;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
 import java.util.List;
 import kr.tennispark.activity.admin.infrastructure.repository.ActivityRepository;
 import kr.tennispark.activity.common.domain.Activity;
+import kr.tennispark.activity.common.domain.vo.WeekPeriod;
 import kr.tennispark.notification.application.FcmMessageService;
 import kr.tennispark.notification.application.NotificationMessageFactory;
 import lombok.RequiredArgsConstructor;
@@ -18,24 +17,24 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class SendRecruitReminderScheduler {
 
+    private final Integer DAYS_TO_ADD = 6;
     private final FcmMessageService fcmMessageService;
     private final ActivityRepository activityRepository;
 
-    @Scheduled(cron = "0 0 10 ? * MON")
+    @Scheduled(cron = "0 0 10 ? * MON") // 매주 월요일 오전 10시
     @Transactional(readOnly = true)
     public void sendRecruitReminder() {
-        LocalDate today = LocalDate.now();
-        LocalDate startOfWeek = today.with(DayOfWeek.MONDAY);
-        LocalDate endOfWeek = today.with(DayOfWeek.SUNDAY);
+        WeekPeriod period = WeekPeriod.current();
 
-        List<Activity> activitiesWithVacancy = activityRepository.findThisWeeksVacantActivities(startOfWeek, endOfWeek);
+        List<Activity> activitiesWithVacancy = activityRepository.findThisWeeksVacantActivities(
+                period.start(), period.start().plusDays(DAYS_TO_ADD)
+        );
 
         if (activitiesWithVacancy.isEmpty()) {
             return;
         }
 
         String message = NotificationMessageFactory.createRecruitReminderMessage(activitiesWithVacancy);
-
         fcmMessageService.sendBroadcastMessage(message);
     }
 }
