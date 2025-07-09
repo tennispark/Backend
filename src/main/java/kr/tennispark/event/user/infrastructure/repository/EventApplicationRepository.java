25930c867c76ce926222ef04d463742b00171a18
msk226 <kminseok5167@gmail.com>
2025-07-09 09:27:40 +0900

    feature: 이벤트 별로 하루에 한 번 참여할 수 있도록 검증 로직 수정

--- src/main/java/kr/tennispark/event/user/infrastructure/repository/EventApplicationRepository.java
+++ src/main/java/kr/tennispark/event/user/infrastructure/repository/EventApplicationRepository.java
@@ -1,5 +1,6 @@
 package kr.tennispark.event.user.infrastructure.repository;
 
+import kr.tennispark.event.common.domain.Event;
 import kr.tennispark.event.common.domain.association.EventApplication;
 import kr.tennispark.members.common.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,9 +15,10 @@ public interface EventApplicationRepository extends JpaRepository<EventApplicati
                 SELECT COUNT(ea) > 0
                 FROM EventApplication ea
                 WHERE ea.member = :member
+                  AND ea.event = :event
                   AND DATE(ea.createdAt) = CURRENT_DATE
             """)
-    boolean existsByMemberToday(@Param("member") Member member);
+    boolean existsByMemberToday(@Param("member") Member member, @Param("event") Event event);
 
     long count();
 }
