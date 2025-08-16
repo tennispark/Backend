58945b8ccfa7ce53411d75f493e1e923ba2179f7
yooookm <yookyungmi02@gmail.com>
2025-08-16 18:28:34 +0900

    refactor: FcmMessageService에서 안 쓰는 함수 제거

--- src/main/java/kr/tennispark/notification/application/FcmMessageService.java
+++ src/main/java/kr/tennispark/notification/application/FcmMessageService.java
@@ -6,11 +6,7 @@ import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.MulticastMessage;
 import com.google.firebase.messaging.Notification;
 import com.google.firebase.messaging.SendResponse;
-import io.micrometer.common.util.StringUtils;
 import java.util.List;
-import java.util.stream.Collectors;
-import kr.tennispark.members.common.domain.entity.Member;
-import kr.tennispark.members.user.infrastructure.repository.MemberRepository;
 import kr.tennispark.notification.application.exception.FcmMessageSendFailureException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,31 +20,11 @@ public class FcmMessageService {
     private static final int BATCH_SIZE = 500;
     private static final String TITLE = "Tennis Park";
 
-    private final MemberRepository memberRepository;
-
     public void sendMessage(List<String> tokens, String content) {
         for (int i = 0; i < tokens.size(); i += BATCH_SIZE) {
             List<String> batch = tokens.subList(i, Math.min(i + BATCH_SIZE, tokens.size()));
             sendMulticast(content, batch);
         }
-    }
-
-    public void sendBroadcastMessage(String content) {
-        List<String> fcmTokens = getValidFcmTokens();
-
-        // 500개씩 나눠서 전송
-        for (int i = 0; i < fcmTokens.size(); i += BATCH_SIZE) {
-            List<String> batch = fcmTokens.subList(i, Math.min(i + BATCH_SIZE, fcmTokens.size()));
-            sendMulticast(content, batch);
-        }
-    }
-
-    private List<String> getValidFcmTokens() {
-        return memberRepository.findAll().stream()
-                .map(Member::getFcmToken)
-                .filter(token -> !StringUtils.isBlank(token))
-                .distinct()
-                .collect(Collectors.toList());
     }
 
     private void sendMulticast(String content, List<String> tokens) {
