86556ab351d1edb5125b191ad4f25396aa78a1d7
yooookm <yookyungmi02@gmail.com>
2025-08-16 17:57:17 +0900

    chore: 알림 카테고리 주석 수정

--- src/main/java/kr/tennispark/notification/domain/entity/enums/NotificationCategory.java
+++ src/main/java/kr/tennispark/notification/domain/entity/enums/NotificationCategory.java
@@ -2,8 +2,8 @@ package kr.tennispark.notification.domain.entity.enums;
 
 public enum NotificationCategory {
     ANNOUNCEMENT,     // 공지(브로드캐스트/추가모집)
-    ACTIVITY_GUIDE,   // 활동 안내(승인/리마인더)
-    MATCHING_GUIDE,    // 매칭 안내
+    ACTIVITY_GUIDE,   // 활동 안내(승인)
+    MATCHING_GUIDE,    // 매칭 안내(리마인더)
     COMMUNITY,       // 커뮤니티 알림
     ETC             // 기타
 }
