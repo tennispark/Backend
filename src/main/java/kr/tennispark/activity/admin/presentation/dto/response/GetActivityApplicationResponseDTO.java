dd9142d15f4836594993b385728932cb076ee8c1
yooookm <yookyungmi02@gmail.com>
2025-08-21 21:43:10 +0900

    feat: 관리자 활동 조회 api에 date 필드 제거

--- src/main/java/kr/tennispark/activity/admin/presentation/dto/response/GetActivityApplicationResponseDTO.java
+++ src/main/java/kr/tennispark/activity/admin/presentation/dto/response/GetActivityApplicationResponseDTO.java
@@ -1,6 +1,5 @@
 package kr.tennispark.activity.admin.presentation.dto.response;
 
-import java.time.LocalDate;
 import java.time.LocalTime;
 import kr.tennispark.activity.common.domain.Activity;
 import org.springframework.data.domain.Page;
@@ -14,7 +13,6 @@ public record GetActivityApplicationResponseDTO(
                         activity.getId(),
                         activity.getPlace().getName(),
                         activity.getCourtName(),
-                        activity.getDate(),
                         activity.getScheduledTime().getBeginAt(),
                         activity.getScheduledTime().getEndAt(),
                         activity.getApplicantCount(),
@@ -28,7 +26,6 @@ public record GetActivityApplicationResponseDTO(
             Long id,
             String placeName,
             String courtName,
-            LocalDate date,
             LocalTime startAt,
             LocalTime endAt,
             int participantCount,
@@ -38,14 +35,12 @@ public record GetActivityApplicationResponseDTO(
                 Long id,
                 String placeName,
                 String courtName,
-                LocalDate date,
                 LocalTime startAt,
                 LocalTime endAt,
                 int participantCount,
                 int capacity
         ) {
-            return new ActivityApplicationDTO(id, placeName, courtName, date, startAt, endAt, participantCount,
-                    capacity);
+            return new ActivityApplicationDTO(id, placeName, courtName, startAt, endAt, participantCount, capacity);
         }
 
     }
