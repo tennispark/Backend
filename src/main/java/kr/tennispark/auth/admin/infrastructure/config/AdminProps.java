f32adc4cdf29d34ca37a8a301553fae05727de9b
sh9406 <98435005+sh9406@users.noreply.github.com>
2026-04-13 20:31:22 +0900

    관리자 지역별 계정 추가

--- src/main/java/kr/tennispark/auth/admin/infrastructure/config/AdminProps.java
+++ src/main/java/kr/tennispark/auth/admin/infrastructure/config/AdminProps.java
@@ -1,7 +1,9 @@
 package kr.tennispark.auth.admin.infrastructure.config;
 
+import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "security.admin")
-public record AdminProps(String id, String password) {
+@ConfigurationProperties(prefix = "security")
+public record AdminProps(List<AdminAccount> admins) {
+    public record AdminAccount(String id, String password, String role, String region) {}
 }
